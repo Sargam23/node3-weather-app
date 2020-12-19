70096e93d7ad549f157836aaa5d4c2b3ed9b6e98
PixelateNow <65773193+PixelateNow@users.noreply.github.com>
2020-12-19 19:33:20 +0530

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,10 +6,7 @@ const forecase=require('./utils/forecast')
 const forecast = require("./utils/forecast")
 
 const app=express()
-
-console.log(__dirname)
-console.log(path.join(__dirname,"../public"))
-
+const port=process.env.PORT ||3000
 
 //define paths
 const publicdirectorypath=path.join(__dirname,"../public")
@@ -107,7 +104,7 @@ app.get("*",(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(port,()=>{
+    console.log("Server is up on port "+port)
 
 })
